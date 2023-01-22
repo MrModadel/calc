43bff95b985fd752e1c5d612b6c961de0852ca39
MrModadel <timanormu@gmail.com>
2023-01-22 19:30:06 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let a = parseInt(prompt("Введите первое значение!"));
+let cut = prompt("Введите одно из значение + - * /");
+let b = parseInt(prompt("Введите второе значение!"));
+let qe = a + b;
+let rr = a - b;
+let ew = a * b;
+let wy = a / b;
+console.log(qe, rr , ew , wy);
+let message = (cut === '+') ? qe :
+   (cut === '-') ? rr :
+      (cut === '*') ? ew :
+         wy;
+alert(message);
